92d16c67bf82f55d3c489ee3abaa15b12e08af1c
cameronKC <cameronleekcmo@gmail.com>
2021-12-23 19:24:54 -0600

    react toastify
    sign in and sign up error messages

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 import {
   getAuth,
@@ -55,7 +56,7 @@ function SignUp() {
 
       navigate("/");
     } catch (error) {
-      console.log(error);
+      toast.error("Sorry, something went wrong!");
     }
   };
 
